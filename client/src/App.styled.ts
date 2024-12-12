b2a779fc83bf0c0fde81857206afdb2968c5ccba
Nir Krumer <nirkrumer@Nirs-MBP.lan>
2024-12-12 21:10:09 +0200

    last

--- client/src/App.styled.ts
+++ client/src/App.styled.ts
@@ -3,9 +3,9 @@ import { styled } from '@stitches/react';
 export const appWraper = styled('div', {
     display: 'flex',
     flexDirection: 'column',
-    gap: '2rem'
+    gap: '3rem'
 })
 export const combinationListWraper = styled('div', {
     display: 'flex',
-    gap: '1rem'
+    gap: '3rem'
 })
